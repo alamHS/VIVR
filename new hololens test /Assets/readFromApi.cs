62e82f666641243ee5adfc891095234e2474024c
Hibah Alam <halam2@uic.edu>
2019-05-02 17:43:03 -0500

    gfkjgjdrytufkyig

---
+++ new hololens test /Assets/readFromApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class readFromApi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         string text = File.ReadAllText("example.txt");
+         Console.WriteLine(text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
